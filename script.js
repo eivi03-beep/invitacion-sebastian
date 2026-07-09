2f5fec27a04338b100b1919a7e580c537a587291
eivi03-beep <eivi_03@hotmail.com>
2026-07-09 17:25:23 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,77 @@
+// Abrir invitación
+
+const botonAbrir = document.getElementById("abrir");
+const bienvenida = document.getElementById("bienvenida");
+const invitacion = document.getElementById("invitacion");
+
+
+botonAbrir.addEventListener("click", () => {
+
+    bienvenida.style.display = "none";
+    invitacion.style.display = "block";
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+});
+
+
+// Cuenta regresiva
+
+const fechaEvento = new Date("August 25, 2026 15:00:00").getTime();
+
+
+const contador = setInterval(function(){
+
+    const ahora = new Date().getTime();
+
+    const diferencia = fechaEvento - ahora;
+
+
+    const dias = Math.floor(
+        diferencia / (1000 * 60 * 60 * 24)
+    );
+
+
+    const horas = Math.floor(
+        (diferencia % (1000 * 60 * 60 * 24))
+        /
+        (1000 * 60 * 60)
+    );
+
+
+    const minutos = Math.floor(
+        (diferencia % (1000 * 60 * 60))
+        /
+        (1000 * 60)
+    );
+
+
+    const segundos = Math.floor(
+        (diferencia % (1000 * 60))
+        /
+        1000
+    );
+
+
+    document.getElementById("cuenta").innerHTML =
+
+    dias + " días " +
+    horas + " horas " +
+    minutos + " minutos " +
+    segundos + " segundos";
+
+
+    if(diferencia < 0){
+
+        clearInterval(contador);
+
+        document.getElementById("cuenta").innerHTML =
+        "¡Hoy es el gran día! 🎉";
+
+    }
+
+
+},1000);
